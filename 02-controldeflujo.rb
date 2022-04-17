73c28559ef1fd5a3585fd57866c1cc3d5c3f1e2e
Ivan-Venegas-Avila <102393527+Ivan-Venegas-Avila@users.noreply.github.com>
2022-04-16 20:57:49 -0400

    Add files via upload

---
+++ 02-controldeflujo.rb
@@ -0,0 +1,13 @@
+print "ingrese la temperatura: "
+t1=gets.chomp.to_i
+print "llueve o no llueve? "
+c1=gets.chomp
+if t1 <= 25 && c1 == 'si'
+  puts "Hoy me quedo en casa a ver peliculas"
+
+else
+  puts "Lindo dias para pasear"
+
+end
+
+#elsif sirve para condisionar un intermedio#
